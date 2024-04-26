8cef7adccb5a3d514223c0a0dd5da87d61107cbd
Maikon Albuquerque <maikonmorais@gmail.com>
2024-04-26 15:40:21 -0300

    :test_tube: test: inserting new restaurants

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+
+insert into restaurante (nome, taxa_frete) values ('Nam Thai', 10);
+insert into restaurante (nome, taxa_frete) values ('Bawarchi' , 20);
