a190beefc035887fe5f333c56dfac84d5ddc7210
Maikon Albuquerque <maikonmorais@gmail.com>
2024-05-07 18:23:10 -0300

    :sparkles: feat: relacionamento ManyToOne entras as duas entidades e nullable false

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -16,9 +16,14 @@ public class Restaurante {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
-    @Column(name = "taxa_frete")
+    @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
+
+    @ManyToOne
+    @JoinColumn(name = "cozinha_codigo", nullable = false)
+    private Cozinha cozinha;
 
 }
