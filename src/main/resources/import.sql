9cc3820c0e887c7d6a94f7312d4189696116dab4
Maikon Albuquerque <maikonmorais@gmail.com>
2024-05-08 17:22:11 -0300

    :test_tube: test: correção no column cozinha_id

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
 
-insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('Nam Thai', 10, 1);
-insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('Bawarchi' , 20, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Nam Thai', 10, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Bawarchi' , 20, 2);
