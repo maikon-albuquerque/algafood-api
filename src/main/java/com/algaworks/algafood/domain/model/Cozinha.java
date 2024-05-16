91ddf456ce96e464a0e7782dead9c6b819c729ef
Maikon Albuquerque <maikonmorais@gmail.com>
2024-05-16 09:21:26 -0300

    :bug: fix: remoção do JsonProperties

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -16,7 +16,6 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JsonProperty("titulo")
     @Column(nullable = false)
     private String nome;
 
