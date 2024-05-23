05a861e93082c227fb997333f07bf3ad67d2a033
Maikon Albuquerque <maikonmorais@gmail.com>
2024-05-23 17:33:58 -0300

    :sparkles: feat: Adicionando ResponseEntity ao EstadoController

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -5,7 +5,9 @@ import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,12 @@ public class EstadoController {
     @GetMapping
     public List<Estado> listaEstados() {
         return estadoRepository.listar();
+    }
+
+    @GetMapping("/{estadoId}")
+    public ResponseEntity<Estado> buscaEstadoPorId(@PathVariable Long estadoId) {
+        Estado estado = estadoRepository.buscar(estadoId);
+        return ResponseEntity.ok(estado);
     }
 
 }
